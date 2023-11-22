734d1622403d37cc0d269c95292c1fa89515ee1b
obo1412 <obo1412@gmail.com>
2023-11-22 22:40:32 +0900

    아이콘 추가 및 내소개 섹션 처리

--- public/asset/icons/MyIcons.js
+++ public/asset/icons/MyIcons.js
@@ -1,11 +1,35 @@
-import { faUser as faUserReg } from "@fortawesome/free-regular-svg-icons";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser as faUserReg,
+  faCalendarCheck as faCalendarCheckReg,
+  faEnvelope as faEnvelopeReg,
+  faAddressBook as faAddressBookReg,
+} from "@fortawesome/free-regular-svg-icons";
 
+// 메뉴 햄버거 버튼
 export function Bars(size, color) {
   return <FontAwesomeIcon icon={faBars} size={size} color={color} />;
 }
 
+// 사람
 export function User(size, color) {
   return <FontAwesomeIcon icon={faUserReg} size={size} color={color} />;
+}
+
+// 생년월일 앞
+export function Calendar(size, color) {
+  return (
+    <FontAwesomeIcon icon={faCalendarCheckReg} size={size} color={color} />
+  );
+}
+
+// 이메일
+export function Envelope(size, color) {
+  return <FontAwesomeIcon icon={faEnvelopeReg} size={size} color={color} />;
+}
+
+// 주소지
+export function AddressBook(size, color) {
+  return <FontAwesomeIcon icon={faAddressBookReg} size={size} color={color} />;
 }
--- src/app/components/Section2_aboutme.tsx
+++ src/app/components/Section2_aboutme.tsx
@@ -1,28 +1,29 @@
 import React from "react";
+import * as MyIcons from "../../../public/asset/icons/MyIcons";
 
 const Section2 = () => {
   const items = [
     {
       id: 1,
-      icon: "",
+      icon: MyIcons.User("2x"),
       title: "이름",
       content: "오병옥",
     },
     {
       id: 2,
-      icon: "",
+      icon: MyIcons.Calendar("2x"),
       title: "생년월일",
       content: "88.11.05",
     },
     {
       id: 3,
-      icon: "",
+      icon: MyIcons.Envelope("2x"),
       title: "이메일",
       content: "obo1412@gmail.com",
     },
     {
       id: 4,
-      icon: "",
+      icon: MyIcons.AddressBook("2x"),
       title: "주소지",
       content: "서울특별시 강동구",
     },
@@ -30,8 +31,28 @@ const Section2 = () => {
 
   return (
     <div className="bg-slate-300 p-4">
-      <div>ABOUT ME</div>
-      <div></div>
+      <div>
+        <h1>ABOUT ME</h1>
+      </div>
+      <div className="">
+        {items.map((item) => (
+          <div
+            key={item.id}
+            className="flex flex-row justify-start items-center border rounded-md p-2 mt-1"
+          >
+            <div className="mx-4 min-w-[40px] flex justify-center items-center">
+              {item.icon}
+            </div>
+            <div
+              className="flex flex-col justify-center items-center text-[20px] min-w-[240px]"
+              style={{ fontFamily: "PF스타더스트" }}
+            >
+              <span>{item.title}</span>
+              <span>{item.content}</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
