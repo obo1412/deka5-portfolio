485ae38e3178191e687716e3b17ceca8d4da00cd
obo1412 <obo1412@gmail.com>
2023-11-22 20:47:25 +0900

    탑바 작업 완료

--- src/app/components/TopNavBar.tsx
+++ src/app/components/TopNavBar.tsx
@@ -4,6 +4,29 @@ import React, { useState } from "react";
 import Link from "next/link";
 
 const TopNavBar = () => {
+  const menuArr = [
+    {
+      id: 1,
+      name: "About Me",
+    },
+    {
+      id: 2,
+      name: "Skills",
+    },
+    {
+      id: 3,
+      name: "Archiving",
+    },
+    {
+      id: 4,
+      name: "Projects",
+    },
+    {
+      id: 5,
+      name: "Career",
+    },
+  ];
+
   const [openMenu, setOpenMenu] = useState(false);
 
   const clickedMenuButton = () => {
@@ -12,35 +35,44 @@ const TopNavBar = () => {
 
   return (
     <nav
-      className="flex flex-row flex-0 justify-between items-center bg-gray-800 bg-opacity-80 text-white px-4 w-full h-[60px] fixed top-0 z-10"
+      className="flex flex-col sm:flex-row justify-between items-center bg-gray-800 bg-opacity-80 text-white w-full fixed top-0 z-10"
       style={{
         fontFamily: "PF스타더스트",
       }}
     >
-      <div className="w-[25%]">
-        <Link href="/" className="text-[40px]">
-          Home
-        </Link>
-      </div>
-      <div className="flex sm:hidden">
-        <button
-          type="button"
-          className="border rounded-lg p-2 flex justify-center items-center w-[50px] hover:bg-slate-500"
-          onClick={clickedMenuButton}
-        >
-          <span className="w-[24px] h-[16px] bg-red-600 hover:bg-red-400 rounded-md"></span>
-        </button>
+      <div className="flex flex-row justify-between w-full">
+        <div className="flex justify-center items-center p-1 ml-3">
+          <Link
+            href="/"
+            className="text-[36px] hover:border rounded-md p-1 hover:bg-slate-500 hover:bg-opacity-50"
+          >
+            Home
+          </Link>
+        </div>
+        <div className="flex sm:hidden p-4">
+          <button
+            type="button"
+            className="border rounded-lg p-2 flex justify-center items-center w-[50px] hover:bg-slate-500"
+            onClick={clickedMenuButton}
+          >
+            <span className="w-[24px] h-[24px] bg-red-600 hover:bg-red-400 rounded-full"></span>
+          </button>
+        </div>
       </div>
       <div
         className={`${
           openMenu && "hidden"
-        } sm:flex flex-col sm:flex-row flex-1 justify-evenly font-bold`}
+        } flex sm:flex flex-col sm:flex-row justify-evenly font-bold w-full sm:min-w-[440px] px-2`}
       >
-        <Link href="/">About Me</Link>
-        <Link href="/">Skills</Link>
-        <Link href="/">Archiving</Link>
-        <Link href="/">Projects</Link>
-        <Link href="/">Career</Link>
+        {menuArr.map((menuItem) => (
+          <Link
+            href="/"
+            key={menuItem.id}
+            className="my-1 hover:border p-2 rounded-md hover:bg-slate-500 hover:bg-opacity-50"
+          >
+            {menuItem.name}
+          </Link>
+        ))}
       </div>
     </nav>
   );
