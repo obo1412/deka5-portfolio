92b14b3cd8ab3d2d9841019ce022eb506d46cbd0
obo1412 <obo1412@gmail.com>
2023-11-28 23:21:50 +0900

    엔지니어 파트 스킬 추가

--- src/app/components/Section3_skills.tsx
+++ src/app/components/Section3_skills.tsx
@@ -3,6 +3,30 @@ import * as MyIcons from "../../../public/asset/icons/MyIcons";
 import Image from "next/image";
 
 const Section3 = ({ refMove, title }: any) => {
+  const engineerPart: any = {
+    Design: [
+      {
+        id: 1,
+        title: "PTC Creo",
+        img: "/asset/image/engineer/design/creo.png",
+        showLabel: false,
+      },
+      {
+        id: 2,
+        title: "CATIA",
+        img: "/asset/image/engineer/design/catia.png",
+        showLabel: false,
+      },
+    ],
+    Analysis: [
+      {
+        id: 1,
+        title: "ANSYS",
+        img: "/asset/image/engineer/analysis/ansys.png",
+        showLabel: false,
+      },
+    ],
+  };
   const skillstack: any = {
     FrontEnd: [
       {
@@ -158,41 +182,83 @@ const Section3 = ({ refMove, title }: any) => {
         <h1 className="mr-4">{title.name.toUpperCase()}</h1>
         <span>{title.icon}</span>
       </div>
-      <div className="columns-1 md:columns-2 lg:columns-3 gap-4">
-        {Object.keys(skillstack).map((key, index) => (
-          <div
-            key={index}
-            className={`${key} bg-white rounded-lg p-2 flex flex-1 flex-col justify-start items-center px-10 mb-4 break-inside-avoid hover:-translate-y-1`}
-          >
-            <div className="text-[34px] text-[#c99700] border-b-2 w-full flex justify-center">
-              <span>{key}</span>
-            </div>
-            {skillstack[key].map((item: any) => (
-              <div
-                key={item.id}
-                className="flex justify-center items-center w-full"
-              >
-                <div
-                  className="h-[80px] flex-1 min-w-[100px] mt-2"
-                  style={{ position: "relative" }}
-                >
-                  <Image
-                    src={item.img}
-                    alt={`logo-${item.title}`}
-                    fill
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                    style={{ objectFit: "contain" }}
-                  />
-                </div>
-                {item.showLabel && (
-                  <span className="flex-grow flex justify-center items-center font__san font-bold text-[40px]">
-                    {item.title}
-                  </span>
-                )}
+      <div className="flex flex-col">
+        <span className="text-[30px] text-center mb-2">- Engineer Part -</span>
+        <div className="columns-1 md:columns-2 lg:columns-3 gap-4 mb-8">
+          {Object.keys(engineerPart).map((key, index) => (
+            <div
+              key={index}
+              className={`${key} bg-white rounded-lg p-2 flex flex-1 flex-col justify-start items-center px-10 mb-4 break-inside-avoid hover:-translate-y-1`}
+            >
+              <div className="text-[34px] text-[#c99700] border-b-2 w-full flex justify-center">
+                <span>{key}</span>
               </div>
-            ))}
-          </div>
-        ))}
+              {engineerPart[key].map((item: any) => (
+                <div
+                  key={item.id}
+                  className="flex justify-center items-center w-full"
+                >
+                  <div
+                    className="h-[80px] flex-1 min-w-[100px] mt-2"
+                    style={{ position: "relative" }}
+                  >
+                    <Image
+                      src={item.img}
+                      alt={`logo-${item.title}`}
+                      fill
+                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                      style={{ objectFit: "contain" }}
+                    />
+                  </div>
+                  {item.showLabel && (
+                    <span className="flex-grow flex justify-center items-center font__san font-bold text-[40px]">
+                      {item.title}
+                    </span>
+                  )}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <span className="text-[30px] text-center mb-2">- Developer Part -</span>
+        <div className="columns-1 md:columns-2 lg:columns-3 gap-4">
+          {Object.keys(skillstack).map((key, index) => (
+            <div
+              key={index}
+              className={`${key} bg-white rounded-lg p-2 flex flex-1 flex-col justify-start items-center px-10 mb-4 break-inside-avoid hover:-translate-y-1`}
+            >
+              <div className="text-[34px] text-[#c99700] border-b-2 w-full flex justify-center">
+                <span>{key}</span>
+              </div>
+              {skillstack[key].map((item: any) => (
+                <div
+                  key={item.id}
+                  className="flex justify-center items-center w-full"
+                >
+                  <div
+                    className="h-[80px] flex-1 min-w-[100px] mt-2"
+                    style={{ position: "relative" }}
+                  >
+                    <Image
+                      src={item.img}
+                      alt={`logo-${item.title}`}
+                      fill
+                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                      style={{ objectFit: "contain" }}
+                    />
+                  </div>
+                  {item.showLabel && (
+                    <span className="flex-grow flex justify-center items-center font__san font-bold text-[40px]">
+                      {item.title}
+                    </span>
+                  )}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
