e18d65e1aa9fa2c9c789250b832bd2cf65eafb11
obo1412 <obo1412@gmail.com>
2023-11-29 15:58:04 +0900

    nextjs image 외부링크에서 불러오기 위한 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "deka.co.kr",
+        port: "8527",
+        pathname: "/files/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
