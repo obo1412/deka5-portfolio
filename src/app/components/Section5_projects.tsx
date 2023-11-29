81a0a182b1cb2e935ff1100533ca37154b3bfb91
obo1412 <obo1412@gmail.com>
2023-11-29 17:31:11 +0900

    프로젝트파트 부분 화면 작업중

--- src/app/components/Section5_projects.tsx
+++ src/app/components/Section5_projects.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import * as MyIcons from "../../../public/asset/icons/MyIcons";
 import ContentProject from "./content/content_section5_project";
+import Image from "next/image";
 
 const Section5 = ({ refMove, title }: any) => {
   return (
@@ -12,14 +13,57 @@ const Section5 = ({ refMove, title }: any) => {
       {Object.keys(ContentProject).map((key, index) => (
         // 객체 내용
         <div key={index} className="flex flex-col items-center px-4">
-          {ContentProject[key as keyof typeof ContentProject]?.map((item) => (
-            <div
-              key={item.id}
-              className="bg-white rounded-lg w-full max-w-[1024px] min-h-[300px] mb-8 p-4"
-            >
-              {item.title}
-            </div>
-          ))}
+          <div className="text-white text-[30px] mb-4">- {key} Part -</div>
+          {ContentProject[key as keyof typeof ContentProject]?.map(
+            (item: any) => (
+              <div
+                key={item.id}
+                className="bg-white rounded-lg w-full max-w-[1024px] min-h-[300px] mb-8 p-4 flex flex-col items-center"
+              >
+                <h1 className="font__blackHanSans text-[36px]">{item.title}</h1>
+                <div className="columns-1 md:columns-2">
+                  <div className="w-full mx-2">
+                    {/* 이미지 파트 왼쪽 */}
+                    {item.img.map((imgItem: any, index: number) => (
+                      <div
+                        key={index}
+                        style={{ position: "relative" }}
+                        className="flex-1 h-[200px] my-1"
+                      >
+                        <Image
+                          src={imgItem.fileDir}
+                          alt={imgItem.fileName}
+                          fill
+                          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                          style={{ objectFit: "contain" }}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                  <div className="flex-1 mx-2 break-inside-avoid">
+                    {/* 정보 파트 오른쪽 */}
+                    <div className="font__san text-[16px]">
+                      {/* 설명 파트 */}
+                      {item.description}
+                    </div>
+                    <div className="border-t mt-2">
+                      {/* 요약 파트 */}
+                      {Object.keys(item.detail).map((subKey, jndex) => {
+                        if (item.detail[subKey] !== "") {
+                          return (
+                            <div key={jndex}>
+                              <span>{subKey}:</span>
+                              <span>{item.detail[subKey]}</span>
+                            </div>
+                          );
+                        }
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          )}
         </div>
       ))}
     </div>
--- src/app/components/content/content_section5_project.js
+++ src/app/components/content/content_section5_project.js
@@ -1,34 +1,54 @@
 const ContentProject = {
-  engineer: [
+  Engineer: [
     {
       id: 1,
       title: "CHC - Engine Cylinder Head Cover",
       date: "2012.11.05 ~ 2018.06.01",
-      img: [],
       description:
-        "현재도 서비스 되고 있는 작은 도서관용 도서 대출/반납 웹프로그램입니다.",
-      point: "도서등",
-      url: "http://finebook.co.kr",
-      frontend: "Spring",
-      backend: "JAVA - Spring 전자정부프레임워크",
-      database: "Mysql - MariaDB",
-      deployment: "Docker - Synology NAS",
+        "엔진 상단을 그저 가리기만 하는 '뷰티커버'와는 다릅니다. 헤드커버는 내연기관 엔진의 최상단에 위치하며, 실린더 캠샤프트를 보호하고 내연기관의 오염된 불완전연소(블로바이) 가스를 다시 재순환 시키는 역할을 하고 있습니다. 헤드커버를 설계하고 연구하며, 필터와 순환장치(벤틸레이션)에 대해 깊게 공부하게 되었습니다. 금형/사출 장치에 대해서도 ",
+      detail: {
+        point: "내연기관 구조 및 가스유동해석으로 결과 예측",
+        url: "",
+        designTool: "Creo",
+        analysisTool: "Ansys - Structure, Fluent",
+        frontend: "",
+        backend: "",
+        database: "",
+        deployment: "",
+      },
+      img: [
+        {
+          id: 1,
+          fileName: "water-pressure-measure",
+          fileDir:
+            "https://deka.co.kr:8527/files/portfolio-deka5/project/engineer/water-pressure.png",
+        },
+        {
+          id: 2,
+          fileName: "water-pressure-measure",
+          fileDir:
+            "https://deka.co.kr:8527/files/portfolio-deka5/project/engineer/water-pressure.png",
+        },
+      ],
     },
   ],
-  developer: [
+  Developer: [
     {
       id: 1,
       title: "FineBook4",
       date: "2019.02.01 ~",
-      img: [],
       description:
         "현재도 서비스 되고 있는 작은 도서관용 도서 대출/반납 웹프로그램입니다.",
-      point: "도서등",
-      url: "http://finebook.co.kr",
-      frontend: "Spring",
-      backend: "JAVA - Spring 전자정부프레임워크",
-      database: "Mysql - MariaDB",
-      deployment: "Docker - Synology NAS",
+      detail: {
+        point:
+          "도서 등록/대출/반납/관리(도서 십진분류 정리, 장서점검 등) 기능을 갖춘 도서관리 프로그램입니다.",
+        url: "http://finebook.co.kr",
+        frontend: "Spring",
+        backend: "JAVA - Spring 전자정부프레임워크",
+        database: "Mysql - MariaDB",
+        deployment: "Docker - Synology NAS",
+      },
+      img: [],
     },
   ],
 };
