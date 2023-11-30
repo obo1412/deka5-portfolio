861cf8d0874612173723d10e48957399d7c9faf9
obo1412 <obo1412@gmail.com>
2023-11-30 21:12:54 +0900

    carousel = swiper 화면 구현

--- src/app/components/Section5_projects.tsx
+++ src/app/components/Section5_projects.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import * as MyIcons from "../../../public/asset/icons/MyIcons";
 import ContentProject from "./content/content_section5_project";
 import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import "../../../public/asset/css/project-swiper.css";
 
 const Section5 = ({ refMove, title }: any) => {
   return (
@@ -21,29 +26,34 @@ const Section5 = ({ refMove, title }: any) => {
                 className="bg-white rounded-lg w-full max-w-[1024px] min-h-[300px] mb-8 p-4 flex flex-col items-center"
               >
                 <h1 className="font__blackHanSans text-[36px]">{item.title}</h1>
-                <div className="columns-1 md:columns-2">
-                  <div className="w-full mx-2">
+                <div className="columns-1 md:columns-2 w-full h-full mt-2">
+                  <div className="mx-2 w-full h-full break-inside-avoid">
                     {/* 이미지 파트 왼쪽 */}
                     {item.img.length > 0 ? (
-                      item.img.map((imgItem: any, index: number) => (
-                        <div
-                          key={index}
-                          style={{ position: "relative" }}
-                          className="flex-1 h-[200px] my-1"
-                        >
-                          <Image
-                            src={imgItem.fileDir}
-                            alt={imgItem.fileName}
-                            fill
-                            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                            style={{ objectFit: "contain" }}
-                          />
-                        </div>
-                      ))
+                      <Swiper
+                        navigation={true}
+                        modules={[Navigation]}
+                        className="my__swiper"
+                      >
+                        {item.img.map((imgItem: any, index: number) => (
+                          <SwiperSlide
+                            key={index}
+                            className="my__swiper__slide"
+                          >
+                            <Image
+                              src={imgItem.fileDir}
+                              alt={imgItem.fileName}
+                              fill
+                              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                              style={{ objectFit: "contain" }}
+                            />
+                          </SwiperSlide>
+                        ))}
+                      </Swiper>
                     ) : (
                       <div
                         style={{ position: "relative" }}
-                        className="flex-1 h-[200px] my-1"
+                        className="flex-1 h-[300px] my-1"
                       >
                         <Image
                           src={
